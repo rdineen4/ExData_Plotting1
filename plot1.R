0203e00b682c4b9b5ff6af4e62c07b7695f5b0c0
rdineen4 <rdineen4@gmail.com>
2020-07-28 10:08:04 -0400

    Finished plot1.R

--- plot1.R
+++ plot1.R
@@ -5,3 +5,21 @@ download.file(fileURL, destfile = "./epcdata")
 #Unzip data file
 unzip("epcdata")
 
+#Read in data from 2007-02-01 and 2007-02-02
+dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, na.strings = "?")
+
+#Convert dates to Date format
+library(lubridate)
+dat$Date <- dmy(dat$Date)
+
+#Subset the desired dates
+datsub <- subset(dat, (Date == "2007-02-01" | Date == "2007-02-02"))
+
+#Open png graphics device
+png(filename = "plot1.png")
+
+#Create Histogram
+hist(datsub$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+#Close png graphics device
+dev.off()
